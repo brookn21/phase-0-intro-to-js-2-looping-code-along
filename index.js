3b98ae3a0146b8f19ae9088dfeeda8b9c09ee2f6
Brook Nega <brooknega21@gmail.com>
2022-09-08 13:22:22 -0400

    commit tree 20c7b82f7eaffa58d0fabbbf3afcf29788a1c9ff

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }
+
+  /*
+  In index.js, build a function named writeCards() that accepts two arguments: an array of string names, and an event name. Create a for loop with a counter that starts at 0 and increments at the end of each loop. The for loop should stop once it has iterated over the length of the array.
+  */
+
+  function writeCards(arr, event){
+    let newArr = []
+    for(let i = 0; i < arr.length; i++){
+      newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr
+  }
+
+  function countDown(num){
+    let i = 0
+    while(num >= i){
+      console.log(num)
+      num--
+    }
+  }
+  countDown(10)
+
+  // function countDown(num){
+  //   let i = 0
+  //   while( i < num){
+  //     console.log(num = num - 1)
+  //     num = num - 1
+  //   }
+  // }
+  // countDown(10)
